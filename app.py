813e50f0e51583ff16be640c624557136be42fed
Gustavo <gustavo.medsch@gmail.com>
2025-09-28 14:56:09 -0300

    Ajustes na classe principal

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from classe_produto import Produto
 
+listaProdutos = []
+categorias = ("Camiseta", "Camisa", "Blusa", "Regata")
+tamanhos = ("PP", "P", "M", "G", "GG", "XG", "XGG", "EG")
 
-def cadastrarProduto():
-    print("==============|CADASTRAR")
-    produto = Produto()
 
+def adicionarMarca( produto:Produto ):
     while True: # PEGAR E VALIDAR NOME
-        nome = input("Qual o nome do produto?\n: ")
+        marca = input("Qual a marca do produto?\n: ")
         try:
-            produto.nome = nome
+            produto.marca = marca
             break
         except ValueError as erro:
             print(f"ERRO: { erro }")
+    return produto.marca
 
+def adicionarPreco( produto:Produto ):
     while True: # PEGAR E VALIDAR PREÇO
         preco = input("Qual o preço do produto?\n: ")
         try:
@@ -20,9 +23,10 @@ def cadastrarProduto():
             break
         except ValueError as erro:
             print(f"ERRO: { erro }")
-
+    return produto.preco
+    
+def adicionarTamanho( produto:Produto ):
     while True: # PEGAR E VALIDAR TAMANHO
-        tamanhos = ("PP", "P", "M", "G", "GG", "XG", "XGG", "EG")
         print("Qual o tamanho do produto?")
         for indice,tamanho in enumerate( tamanhos, start=1 ):
             print(f"({ indice }) - { tamanho }")
@@ -32,7 +36,22 @@ def cadastrarProduto():
             break
         except ValueError as erro:
             print(f"ERRO: { erro }")
-    
+    return produto.tamanho
+
+def adicionarCategoria( produto:Produto ):
+    while True:
+        print("Qual a categoria do produto?")
+        for indice,tamanho in enumerate( categorias, start=1 ):
+            print(f"({ indice }) - { tamanho }")
+        categoria = input(": ")        
+        try: 
+            produto.categoria = categoria
+            break
+        except ValueError as erro:
+            print(f"ERRO: { erro }")
+    return produto.categoria
+
+def adicionarQuantidade( produto:Produto ):
     while True: # PEGAR E VALIDAR QUANTIDADE
         quantidade = input("Qual a quantidade do produto?\n: ")
         try:
@@ -40,32 +59,97 @@ def cadastrarProduto():
             break
         except ValueError as erro:
             print(f"ERRO: { erro }")
-            
-    produto.produtoId = gerarId()
+    return produto.quantidade
+
+def adicionarDescricao( produto:Produto ):
+    while True:
+        descricao = input("Dê uma breve descrição do produto. (até 20 caracteres)\n: ")
+        try:
+            produto.descricao = descricao
+            break
+        except ValueError as erro:
+            print(f"ERRO: { erro }")
+    return produto.descricao
+
+def cadastrarProdutos():
+    print("==============|CADASTRAR PRODUTOS")
+    produto = Produto("","","","","","","")
+
+    marca = adicionarMarca( produto )
+    preco = adicionarPreco( produto )
+    tamanho = adicionarTamanho( produto )
+    categoria = adicionarCategoria( produto )
+    quantidade = adicionarQuantidade( produto )
+    descricao = adicionarDescricao( produto )
+                         
+    produto.produtoId = Produto.gerarIdProduto()
     
-    confirmacao = input("Deseja salvar? (Digite 'sim' para salvar ou qualquer tecla para sair)\n:")
-    if confirmacao.upper() == "SIM":
-        produto.salvar()
+    confirmacao = input("Deseja salvar? (Digite 's' para salvar ou qualquer tecla para sair)\n: ")
+    if confirmacao.upper() == "S":
+        listaProdutos.append(Produto( produto.produtoId,marca,preco,tamanho,categoria,quantidade,descricao ))
+        produto.salvarProduto()
         print("SUCESSO: Produto cadastrado!")
     else:
         print("CANCELADO: Produto não cadastrado.")
         return 
 
-def gerarId():
-    if Produto.produtosArmazenados:
-        return len(Produto.produtosArmazenados) + 1
-    else:
-        return 1
-
 def verProdutos():
-    print("==============|PRODUTOS")
-    if Produto.produtosArmazenados:
-        for produto in Produto.produtosArmazenados:
-            print(f"\n{ produto }")
+    print("==============|VER PRODUTOS")
+    if listaProdutos:
+        for numero,_ in enumerate( listaProdutos,start=0 ):
+            print(f"\n{listaProdutos[numero]}")
         return
     else:
         print("\n(Não há produtos cadastrados)")
 
+def editarProdutos():
+    print("==============|EDITAR PRODUTOS")
+    if Produto.produtosArmazenados:
+        for produto in listaProdutos:
+            print(f"\n{produto}")
+        while True:
+            try: 
+                editarSelecao = int(input("\nQual produto você deseja editar? (Informe o ID)\n: "))
+                id = editarSelecao -1
+                if editarSelecao < 1 or editarSelecao > len( listaProdutos ):
+                    print("ERRO: O ID digitado não existe")
+                else:
+                    while True:
+                        print(f"==============|PRODUTO ID #{id+1}\n")
+                        try:
+                            listaProdutos[ id ].mostrarItensProduto()
+                            opcao = int(input(f"\nO que deseja editar desse produto? (Digite o numero referente ao item)\n: "))
+                            if opcao == 1:
+                                adicionarMarca( listaProdutos[ id ] )                
+                            elif opcao == 2:
+                                adicionarPreco( listaProdutos[ id ] )
+                            elif opcao == 3:
+                                adicionarTamanho( listaProdutos[ id ] )
+                            elif opcao == 4:
+                                adicionarCategoria( listaProdutos[ id ] )
+                            elif opcao == 5:
+                                adicionarQuantidade( listaProdutos[ id ] )
+                            elif opcao == 6:
+                                adicionarDescricao( listaProdutos[ id ] )
+                            else:
+                                print("ERRO: A opção escolhida não existe")
+                                
+                            continuarEditando = str(input("Deseja editar mais algum item do seu produto? (Digite 's' para salvar ou qualquer tecla para sair)\n: "))
+                            if continuarEditando.upper() == "S":
+                                pass
+                            else:
+                                print("SUCESSO: Produto editado!")
+                                break 
+                        except ValueError:
+                            print("ERRO: O valor digitado não é um inteiro")
+                break
+            except ValueError:
+                print("ERRO: O valor digitado não é um inteiro")
+        
+        
+        
+    else:
+        print("\n(Não há produtos cadastrados)")
 
 # Iniciando APP - Loja de roupa
 
@@ -74,6 +158,7 @@ while True:
     print("0 - Sair")
     print("1 - Cadastrar Produtos")
     print("2 - Ver Produtos")
+    print("3 - Editar Produtos")
     
     try:
         opcaoSelecionada = int(input(": "))      
@@ -81,9 +166,11 @@ while True:
             print("\nSaindo do programa...\n")
             break
         elif opcaoSelecionada == 1:            # CADASTRAR
-            cadastrarProduto()
+            cadastrarProdutos()
         elif opcaoSelecionada == 2:            # VER PRODUTOS
             verProdutos()
+        elif opcaoSelecionada == 3:            # EDITAR PRODUTOS
+            editarProdutos()
         else:
             print("\nOPÇÃO INVÁLIDA.\n")
         
--- classe_produto.py
+++ classe_produto.py
@@ -1,12 +1,19 @@
 class Produto:
     produtosArmazenados = []
-    
-    def __init__( self ):
-        pass
+    categorias = ("Camiseta", "Camisa", "Blusa", "Regata")
+
+    def __init__( self,produtoID,marca,preco,tamanho,categoria,quantidade,descricao ):
+        self.produtoId = produtoID
+        self._marca = marca
+        self._preco = preco
+        self._tamanho = tamanho
+        self._categoria = categoria
+        self._quantidade = quantidade
+        self._descricao = descricao
     
     @property
-    def nome( self ):
-        return self._nome
+    def marca( self ):
+        return self._marca
     
     @property
     def preco( self ):
@@ -17,6 +24,10 @@ class Produto:
         return self._tamanho
     
     @property
+    def categoria(self):
+        return self._categoria
+    
+    @property
     def quantidade( self ):
         return self._quantidade
     
@@ -24,12 +35,20 @@ class Produto:
     def produtoId( self ):
         return self._produtoId
     
-    @nome.setter
-    def nome( self,produto_nome ):
-        if len( produto_nome ) <= 3:
+    @property
+    def descricao( self ):
+        return self._descricao
+    
+    @property
+    def precoDesconto( self ):
+        return self._precoDesconto
+    
+    @marca.setter
+    def marca( self,produto_marca ):
+        if len( produto_marca ) <= 3:
             raise ValueError("O nome do produto deve conter mais do que três caracteres")
         else:        
-            self._nome = produto_nome     
+            self._marca = produto_marca.upper()
     
     @preco.setter
     def preco( self,produto_preco ):
@@ -40,14 +59,14 @@ class Produto:
         if precoValido <= 0:
             raise ValueError("O valor informado não pode ser menor ou igual a zero")
         else:
-            self._preco = precoValido  
+            self._preco = precoValido
     
     @tamanho.setter
-    def tamanho( self,numeroTamanho ):
+    def tamanho( self,produto_tamanho ):
         tamanhos = ("PP", "P", "M", "G", "GG", "XG", "XGG", "EG")
         try:
-            tamanhoValido = int( numeroTamanho )
-            if tamanhoValido < 1 or tamanhoValido > 8:
+            tamanhoValido = int( produto_tamanho )
+            if tamanhoValido < 1 or tamanhoValido > len( tamanhos ):
                 raise ValueError("O valor informado deve ser entre 1 e 8")
             else:
                 for indice,tamanho in enumerate( tamanhos, start=1 ):
@@ -55,6 +74,20 @@ class Produto:
                         self._tamanho = tamanho
         except ValueError:
             raise ValueError("O valor informado é inválido")
+    
+    @categoria.setter
+    def categoria( self,produto_categoria ):
+        categorias = ("Camiseta", "Camisa", "Blusa", "Regata")
+        try:
+            categoriaValido = int( produto_categoria )
+            if categoriaValido < 1 or categoriaValido > len( categorias ):
+                raise ValueError("")
+            else:
+                for indice,categor in enumerate(categorias, start=1):
+                    if indice == categoriaValido:
+                        self._categoria = categor
+        except ValueError:
+            raise ValueError("A categoria deve ser um numero inteiro")
     
     @quantidade.setter
     def quantidade( self,produto_quantidade ):
@@ -67,13 +100,48 @@ class Produto:
         else:
             self._quantidade = quantidadeValido
             
+    @descricao.setter
+    def descricao( self, produto_descricao ):
+        if len(produto_descricao) == 0 or len(produto_descricao) < 4:
+            raise ValueError("Nenhuma descrição do produto ou descrição muito pequena")
+        elif len(produto_descricao) > 20:
+            raise ValueError("Você excedeu o numero de caracteres")
+        else:
+            self._descricao = produto_descricao
+            
     @produtoId.setter
-    def produtoId( self,id ):
-        self._produtoId = id
+    def produtoId( self,produto_id ):
+        self._produtoId = produto_id
     
     def __str__( self ):
-        return f"- ID: { self.produtoId }\n- Nome: { self.nome }\n- Preço: R${ self.preco }\n- Tamanho: { self.tamanho }\n- Quantidade: { self.quantidade }"
+        return (f"#ID: { self.produtoId }"
+                f"\n- Marca: { self.marca }"
+                f"\n- Preço: R$ { self.preco }"
+                f"\n- Tamanho: { self.tamanho }"
+                f"\n- Categoria: { self.categoria }"
+                f"\n- Quantidade: { self.quantidade }"
+                f"\n- Descrição: { self.descricao }")
+    
+    def gerarIdProduto():
+        if Produto.produtosArmazenados:
+            return len(Produto.produtosArmazenados) + 1
+        else:
+            return 1
 
-    def salvar( self ):
-        Produto.produtosArmazenados.append(self)
+    def salvarProduto( self ):
+        Produto.produtosArmazenados.append( self )
         return
+    
+    def mostrarProdutos():
+        for produto in Produto.produtosArmazenados:
+            print(f"\n{ produto }")
+            
+    def mostrarItensProduto( self ):
+        print(f"(1) - Marca: { self.marca }\n"
+            f"(2) - Preço: { self.preco }\n"
+            f"(3) - Tamanho: { self.tamanho }\n"
+            f"(4) - Categoria: { self.categoria }\n"
+            f"(5) - Quantidade: { self.quantidade }\n"
+            f"(6) - Descrição: { self.descricao }")
+
+    
